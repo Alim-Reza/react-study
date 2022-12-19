ff05b368564209db1c34ab0ba800036ff281860f
alim-ahmed-reza <alim@cokreates.com>
2022-12-20 01:25:16 +0600

    added mui

--- reminder/src/components/user-registration/SignUp.jsx
+++ reminder/src/components/user-registration/SignUp.jsx
@@ -1,7 +1,29 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Card  from '@mui/material/Card';
+import TextField from "@mui/material/TextField";
+
 export default function SignUp() {
     return (
         <>
-            Hello world im sign up
+            <Container maxWidth="sm" sx={{ bgcolor: 'grey', height: '100vh',  }}>
+                    <Card sx={{ height: "100%", padding: "1em"}}>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}>
+                            <TextField id="outlined-basic" label="first name" variant="outlined" style={{margin: "1em"}} />
+                            <TextField id="outlined-basic" label="last name" variant="outlined" style={{margin: "1em"}} />
+                        </div>
+                        <TextField id="outlined-basic" label="phone" variant="outlined" style={{margin: "1em"}} />
+                        <TextField id="outlined-basic" label="email" variant="outlined" style={{margin: "1em"}} />
+                        <TextField id="outlined-basic" label="password" variant="outlined" style={{margin: "1em"}} />
+                        <TextField id="outlined-basic" label="confirm-password" variant="outlined" style={{margin: "1em"}} />
+
+                    </Card>
+            </Container>
         </>
     );
 } 
